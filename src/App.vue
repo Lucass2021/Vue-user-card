ac8eba4b42c950cda68cb4b9f7f757fa30505442
Lucas Dias <lucas.allx@hotmail.com>
2024-01-04 17:04:19 -0300

    initial card

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
-    <h1>Hello World</h1>
+    <div>
+      <CardComponent/>
+    </div>
 </template>
 
 <script setup>
+import CardComponent from './components/CardComponent.vue';
 
 </script>
 
 <style>
+body{
+  background: #1a1a1a;
+}
 </style>
+
+
+<!-- Data:
+https://reqres.in/api/users/
+// const data = [
+//   { id: '1', image: 'https://img.freepik.com/fotos-premium/anime-menino-homem-avatar-ai-arte-generativa_225753-7457.jpg', name: 'Nome 1', email: 'email1@example.com' },
+// ]; -->
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="card">
+            <img src="https://img.freepik.com/fotos-premium/anime-menino-homem-avatar-ai-arte-generativa_225753-7457.jpg" alt="">
+            <h1>Lucas Dias</h1>
+            <h2>lucas.allx@hotmail.com</h2>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+.card{
+    background: #fff;
+    width: 300px;
+}
+
+.card img{
+    max-width: 90%;
+    display: flex;
+    margin:0 auto;
+    padding-top: 15px;
+}
+
+</style>
