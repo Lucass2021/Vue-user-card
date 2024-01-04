51a88b9aa8071dc8a91cde1a9039cf44750ec9ba
Lucas Dias <lucas.allx@hotmail.com>
2024-01-04 18:59:52 -0300

    card component with props

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <template>
     <div>
-      <CardComponent/>
+      <CardComponent 
+      image="https://img.freepik.com/fotos-premium/anime-menino-homem-avatar-ai-arte-generativa_225753-7457.jpg"
+      name="Lucas Dias"
+      email="lucas.allx@hotmail.com"
+      />
     </div>
 </template>
 
 <script setup>
 import 'bootstrap/dist/css/bootstrap.css'
 import CardComponent from './components/CardComponent.vue';
+
+import { ref, onMounted } from "vue"
+
+let userData = ref("")
+
+const handleUserApiRequest = async () => {
+  const req = await fetch(`https://reqres.in/api/users/`)
+  const json = await req.json()
+  userData.value = json.data
+}
+
+onMounted(() => {
+  console.log('O componente foi montado no DOM!');
+  handleUserApiRequest()
+})
 
 </script>
 
--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -3,29 +3,9 @@
         <div class="container">
             <div class="row justify-content-center">
                 <div class="card">
-                    <img src="https://img.freepik.com/fotos-premium/anime-menino-homem-avatar-ai-arte-generativa_225753-7457.jpg" alt="">
-                    <h1>Lucas Dias</h1>
-                    <h2><a href="mailto:lucas.allx@hotmail.com">lucas.allx@hotmail.com</a></h2>
-                </div>
-                <div class="card">
-                    <img src="https://img.freepik.com/fotos-premium/anime-menino-homem-avatar-ai-arte-generativa_225753-7457.jpg" alt="">
-                    <h1>Lucas Dias</h1>
-                    <h2><a href="mailto:lucas.allx@hotmail.com">lucas.allx@hotmail.com</a></h2>
-                </div>
-                <div class="card">
-                    <img src="https://img.freepik.com/fotos-premium/anime-menino-homem-avatar-ai-arte-generativa_225753-7457.jpg" alt="">
-                    <h1>Lucas Dias</h1>
-                    <h2><a href="mailto:lucas.allx@hotmail.com">lucas.allx@hotmail.com</a></h2>
-                </div>
-                <div class="card">
-                    <img src="https://img.freepik.com/fotos-premium/anime-menino-homem-avatar-ai-arte-generativa_225753-7457.jpg" alt="">
-                    <h1>Lucas Dias</h1>
-                    <h2><a href="mailto:lucas.allx@hotmail.com">lucas.allx@hotmail.com</a></h2>
-                </div>
-                <div class="card">
-                    <img src="https://img.freepik.com/fotos-premium/anime-menino-homem-avatar-ai-arte-generativa_225753-7457.jpg" alt="">
-                    <h1>Lucas Dias</h1>
-                    <h2><a href="mailto:lucas.allx@hotmail.com">lucas.allx@hotmail.com</a></h2>
+                    <img :src="props.image" alt="">
+                    <h1>{{ props.name }}</h1>
+                    <h2><a :href="`mailto:${props.email}`">{{ props.email }}</a></h2>
                 </div>
             </div>
         </div>
@@ -33,6 +13,13 @@
 </template>
 
 <script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    image: String,
+    name: String,
+    email: String,
+})
 
 </script>
 
